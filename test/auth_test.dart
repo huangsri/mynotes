f5b73320f09a7c8ff4156a2e4c4a2e2c9d211723
huangsri <peerawas.likit@gmail.com>
2022-04-04 21:00:51 +0700

    c30: preparing to create new note

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -125,7 +125,10 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
 
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(
+      isEmailVerified: false,
+      email: 'huang@wow.com',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -145,7 +148,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: 'huang@wow.com',
+    );
     _user = newUser;
   }
 }
